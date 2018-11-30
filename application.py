4cc1ba8b5190df0a3744bdd59fbc82f26e1f2b99
Chris Catwell <christophercantwell@college.harvard.edu>
2018-11-30 00:06:45 -0500

    Added procfile, reqs to move toward something Heroku can use

--- application.py
+++ application.py
@@ -1,6 +1,14 @@
+from flask import Flask, render_template, request, redirect
+import os
+from cs50 import SQL
+
 def main():
     # Configure application
     app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     main()
