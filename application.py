c6eaf8b0164941a209a9b85984bea7871c7c87ba
Chris Catwell <christophercantwell@college.harvard.edu>
2018-11-30 00:20:40 -0500

    Updates to make Heroku work, including importing CS50 and psycopg

--- application.py
+++ application.py
@@ -2,13 +2,9 @@ from flask import Flask, render_template, request, redirect
 import os
 from cs50 import SQL
 
-def main():
-    # Configure application
-    app = Flask(__name__)
+# Configure application
+app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     return render_template("index.html")
-
-if __name__ == '__main__':
-    main()
