986a31ac184953d266decd20697e98d21f816280
Chris Catwell <christophercantwell@college.harvard.edu>
2018-12-03 01:41:52 -0500

    Fixed image file path, added file processing to the layout template

--- application.py
+++ application.py
@@ -7,27 +7,31 @@ app = Flask(__name__)
 
 db = SQL("postgres://bcbfedessydwun:dbdc8f53173f93f71ff4b7c1ea51fefee2e4dac7d785d312c4f426eae16ce8ab@ec2-54-163-230-178.compute-1.amazonaws.com:5432/dat9s8lnbu4obk")
 
+icon = open('static/ccicon.jpg', 'r')
+
 @app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", icon=icon)
 
 @app.route("/error", methods=["GET"])
 def error():
-    img = open('Partial-Credit.jpg', 'r')
-    return render_template("error.html", img=img)
+    with open('static/Partial-Credit.jpg', 'r') as img:
+        return render_template("error.html", img=img, icon=icon)
 
 @app.route("/input", methods=["POST", "GET"])
 def input():
-    return render_template("error.html")
+    return render_template("dataInput.html", icon=icon)
 
 @app.route("/search", methods=["POST", "GET"])
 def search():
-    return render_template("error.html")
+    return render_template("search.html", icon=icon)
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    return render_template("error.html")
+    return render_template("login.html", icon=icon)
 
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
-    return render_template("error.html")
+    return render_template("error.html", img=img, icon=icon)
+
+icon.close()
