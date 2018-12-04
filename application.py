25b85fc34dacc7ee9cf5292902fc8f27255599a2
Chris Catwell <christophercantwell@college.harvard.edu>
2018-12-03 23:04:21 -0500

    import mkdtemp

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from flask_session import Session
 import os
 from cs50 import SQL
 from functools import wraps
+from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
