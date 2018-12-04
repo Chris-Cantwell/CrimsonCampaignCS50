644191dc00796e5b8c8f079946dfc192fd5463a6
Chris Catwell <christophercantwell@college.harvard.edu>
2018-12-03 22:54:23 -0500

    installed flask_session, updated requirements.txt

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash
 from flask_session import Session
 import os
 from cs50 import SQL
