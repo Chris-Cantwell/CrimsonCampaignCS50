1f6ee6a48402999c87a5c0cadd4748b731de490e
Chris Catwell <christophercantwell@college.harvard.edu>
2018-12-03 23:02:48 -0500

    Trying to solve an issue with cookies and a lack of a secret key

--- application.py
+++ application.py
@@ -12,6 +12,12 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 db = SQL("postgres://bcbfedessydwun:dbdc8f53173f93f71ff4b7c1ea51fefee2e4dac7d785d312c4f426eae16ce8ab@ec2-54-163-230-178.compute-1.amazonaws.com:5432/dat9s8lnbu4obk")
 
 def login_required(f):
