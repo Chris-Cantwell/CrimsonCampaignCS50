391b5022e8d60fdbaf37a8b38d40aba4bd6c88b1
Chris Catwell <christophercantwell@college.harvard.edu>
2018-12-05 20:34:06 -0500

    Multi-line string literal round 2

--- application.py
+++ application.py
@@ -136,48 +136,45 @@ def register():
 
             # Creates a new table based on the campaign type
             if(request.form.get("campaign-type") == "registration"):
-                db.execute("""CREATE TABLE :username (
-                           VoterId int UNIQUE,
-                           FirstName varchar(128),
-                           LastName varchar(128),
-                           House varchar(64),
-                           Entryway varchar(64),
-                           Contact boolean,
-                           State varchar(64),
-                           Hometown varchar(128),
-                           Registered boolean,
-                           Ballot request boolean,
-                           Voted boolean,
-                           Email varchar(255),
-                           Phone varchar(20)
-                           )""", username=user)
+                db.execute("CREATE TABLE :username ("
+                           "VoterId int UNIQUE,"
+                           "FirstName varchar(128),"
+                           "LastName varchar(128),"
+                           "House varchar(64),"
+                           "Entryway varchar(64),"
+                           "Contact boolean,"
+                           "State varchar(64),"
+                           "Hometown varchar(128),"
+                           "Registered boolean,"
+                           "Ballot request boolean,"
+                           "Voted boolean,"
+                           "Email varchar(255),"
+                           "Phone varchar(20)"), username=user)
 
             elif(request.form.get("campaign-type") == "house"):
-                db.execute("""CREATE TABLE :username (
-                           VoterId int UNIQUE,
-                           FirstName varchar(128),
-                           LastName varchar(128),
-                           Entryway varchar(64),
-                           Contact boolean,
-                           Support smallint,
-                           Voted boolean,
-                           Email varchar(255),
-                           Phone varchar(20)
-                           )""", username=user)
+                db.execute("CREATE TABLE :username ("
+                           "VoterId int UNIQUE,"
+                           "FirstName varchar(128),"
+                           "LastName varchar(128),"
+                           "Entryway varchar(64),"
+                           "Contact boolean,"
+                           "Support smallint,"
+                           "Email varchar(255),"
+                           "Voted boolean,"
+                           "Phone varchar(20)"), username=user)
 
             else:
-                db.execute("""CREATE TABLE :username (
-                           VoterId int UNIQUE,
-                           FirstName varchar(128),
-                           LastName varchar(128),
-                           House varchar(64),
-                           Entryway varchar(64),
-                           Contact boolean,
-                           Support smallint,
-                           Voted boolean,
-                           Email varchar(255),
-                           Phone varchar(20)
-                           )""", username=user)
+                db.execute("CREATE TABLE :username ("
+                           "VoterId int UNIQUE,"
+                           "FirstName varchar(128),"
+                           "LastName varchar(128),"
+                           "House varchar(64),"
+                           "Entryway varchar(64),"
+                           "Support smallint,"
+                           "Contact boolean,"
+                           "Voted boolean,"
+                           "Email varchar(255),"
+                           "Phone varchar(20)"), username=user)
 
             # Remember which user has logged in
             rows = db.execute("SELECT id FROM users WHERE username = :username",
